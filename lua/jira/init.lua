699f3cadaf4f0a48dc742ef2e3eeb07f5f16dcf4
Quique Porta <me@quiqueporta.com>
2025-12-28 17:03:44 +0100

    Add JiraIssues command

--- lua/jira/init.lua
+++ lua/jira/init.lua
@@ -9,6 +9,10 @@ M.config = {
 
 function M.setup(opts)
 	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+
+	vim.api.nvim_create_user_command("JiraIssues", function()
+		require("jira.telescope").issues()
+	end, { desc = "Search Jira issues with Telescope" })
 end
 
 return M
